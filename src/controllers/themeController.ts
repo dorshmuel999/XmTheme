cc4d72e29b2620e25833fa770f3d2d5c7e210015
dor.shmuel <dor.shmuel@xmcyber.com>
2025-02-03 12:20:41 +0200

    aa

--- src/controllers/themeController.ts
+++ src/controllers/themeController.ts
@@ -1,40 +1,37 @@
-import { Request, Response } from 'express';
-import { getThemes, getThemeById } from '../models/theme';
-import { v4 } from 'uuid';
-import { getDb } from '../db';
-
+import {Request, Response} from 'express';
+import {getThemes, getThemeById} from '../models/theme';
+import {v4} from 'uuid';
+import {getDb} from '../db';
 
 
 export const listThemes = (req: Request, res: Response) => {
-  const themes = getThemes();
-  res.render('themes', { themes });
+    const themes = getThemes();
+    res.render('themes', {themes});
 };
 
 export const getTheme = (req: Request, res: Response) => {
-  const themeId = parseInt(req.params.id);
-  const theme = getThemeById(themeId);
-  
-  if (!theme) {
-     res.status(404).send('Theme not found');
+    const themeId = parseInt(req.params.id);
+    const theme = getThemeById(themeId);
+
+    if (!theme) {
+        res.status(404).send('Theme not found');
         return;
-  }
-  
-  res.json(theme);
+    }
+
+    res.json(theme);
 };
 
-export const saveTheme = (req: Request, res: Response) => {
-    const themeId = parseInt(v4());
+export const saveTheme = async (req: Request, res: Response) => {
     const theme = req.body;
-      console.log({theme});
-      return;
+    console.log({theme});
     if (!theme) {
-       res.status(400).send('Theme not found');
-          return;
+        res.status(400).send('Theme not found');
+        return;
     }
 
     const db = getDb();
 
-    db.collection('themes').insertOne({ ...theme, id: themeId });
-    
+    await db.collection('themes').insertOne({...theme});
+
     res.json('Theme saved');
-  };
+};
