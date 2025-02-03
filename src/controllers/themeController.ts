03ec69d3eae502c550aca1365f8c4278b15286b4
Amit Klein <amit.klein@xmcyber.com>
2025-02-03 11:52:32 +0200

    add

--- src/controllers/themeController.ts
+++ src/controllers/themeController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express';
 import { getThemes, getThemeById } from '../models/theme';
+import { v4 } from 'uuid';
+import { getDb } from '../db';
 
 
 
@@ -19,3 +21,20 @@ export const getTheme = (req: Request, res: Response) => {
   
   res.json(theme);
 };
+
+export const saveTheme = (req: Request, res: Response) => {
+    const themeId = parseInt(v4());
+    const theme = req.body;
+      console.log({theme});
+      return;
+    if (!theme) {
+       res.status(400).send('Theme not found');
+          return;
+    }
+
+    const db = getDb();
+
+    db.collection('themes').insertOne({ ...theme, id: themeId });
+    
+    res.json('Theme saved');
+  };
