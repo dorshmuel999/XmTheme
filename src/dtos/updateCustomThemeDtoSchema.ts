9aa70f7cd6f9436f3652f9b517a90fecd72c3b19
dor.shmuel <dor.shmuel@xmcyber.com>
2025-02-03 16:32:03 +0200

    aa

--- src/dtos/updateCustomThemeDtoSchema.ts
+++ src/dtos/updateCustomThemeDtoSchema.ts
@@ -1,5 +1,5 @@
 import {z} from 'zod';
 import {customThemeSchemaDto} from "./createCustomThemeDto";
 
-export const updateCustomThemeDtoSchema = customThemeSchemaDto.partial().strict();
+export const updateCustomThemeDtoSchema = customThemeSchemaDto.strict().partial();
 export type UpdateCustomThemeDto = z.infer<typeof updateCustomThemeDtoSchema>;
