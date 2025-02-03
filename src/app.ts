75446f08213f9398a782270210920f6c45fe4c76
Amit Klein <amit.klein@xmcyber.com>
2025-02-03 12:27:19 +0200

    formatting!@%$!

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,23 @@
-import express, {Request, Response} from 'express';
-import themeRoutes from './routes/themRoutes';
-import {connectToDB} from "./db";
+import express, { Request, Response } from "express";
+import themeRoutes from "./routes/themRoutes";
+import { connectToDB } from "./db";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 (async () => {
-    await connectToDB();
+  await connectToDB();
 
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
 
-    app.use(express.json());
-    app.use(express.urlencoded({extended: true}));
+  app.use("/themes", themeRoutes);
 
-    app.use('/themes', themeRoutes);
+  app.get("/", (req: Request, res: Response) => {
+    res.send("Welcome to the theme application!");
+  });
 
-    app.get('/', (req: Request, res: Response) => {
-        res.send('Welcome to the theme application!');
-    });
-
-    app.listen(PORT, () => {
-        console.log(`Server is running on http://localhost:${PORT}`);
-    });
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
 })();
